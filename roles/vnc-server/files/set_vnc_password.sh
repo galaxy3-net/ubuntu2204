c1a3671cd6a1b0d3bd44bfe72b2d385c46cb164f
kirscht <korben@kirscht.com>
2022-08-03 21:08:49 -0700

    python3-dnf

--- roles/vnc-server/files/set_vnc_password.sh
+++ roles/vnc-server/files/set_vnc_password.sh
@@ -3,6 +3,6 @@
 myuser="vagrant"
 mypasswd="uci2020"
 
-echo "${mypasswd}" | vncpasswd -f ${HOME}/$myuser/.vnc/passwd
+echo "${mypasswd}" | vncpasswd -f > ${HOME}/$myuser/.vnc/passwd
 
 chmod 0600 /home/$myuser/.vnc/passwd
